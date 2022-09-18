fc1742db2c23a4348c04fba0534f6ba95a4ee802
Jose Caivano <104044821+zecaivano@users.noreply.github.com>
2022-09-18 10:26:15 -0300

    Publicando o projeto

---
+++ app/validacao.js
@@ -0,0 +1,44 @@
+function verificaSeEhValido(chute) {
+    const numero = +chute; // Só de somar já vai converter o numero para inteiro
+
+    if (chuteInvalido(numero)) {
+       elementoChute.innerHTML += '<div>Valor inválido</div>';
+       return
+    }   
+    if (numeroForaDosLimites(numero)) {
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor} </div>
+        `
+        return
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+      
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForaDosLimites (numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
